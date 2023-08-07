98cbbd77bf40056e062073ff19cb38d4595f76c1
Aniruddha Kibey <aniruddha.kibey@gmail.com>
2023-08-07 09:55:54 +0530

    added tables

--- website/views.py
+++ website/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import Record
 
-# Create your views here.
+def home(request):
+    # Check to see if loggin in
+    records = Record.objects.all().order_by("-created_at").values()
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error logging in ...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {'records': records})
+
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out ...")
+    return redirect('home')
